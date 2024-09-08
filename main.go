4812e0d992d864de3a7d02e977e61169ec72ea22
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-09-08 16:49:36 +0200

    Server setup

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"github.com/joho/godotenv"
+	"net/http"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+
+	port := os.Getenv("PORT")
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		return
+	}
 }
