33364f8b38a2c889826e51c723286b0edb847625
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-09-19 16:32:45 +0200

    Add create feed query

--- sql/schema/003_feeds.sql
+++ sql/schema/003_feeds.sql
@@ -1,12 +1,12 @@
--- +goose up
+-- +goose Up
 CREATE TABLE feeds (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name VARCHAR NOT NULL,
     url VARCHAR NOT NULL UNIQUE,
-    user_id UUID NOT NULL REFERENCES users ON DELETE CASCADE
+    user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
 
--- +goose down
+-- +goose Down
 DROP TABLE feeds;
